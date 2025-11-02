223eb688d46c9053a0d2aaec8a96dfad26b78b93
Williams Abiola <williams.abiola@itech.ng>
2025-11-02 20:19:27 +0100

    Fixed Shareholders Form Submission

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
     if (!authenticate(req)) {
         logger.warn(`Unauthorized access attempt on ${req.path}`);
-        return res.status(401).json({ error: 'Invalid API Key, Version 1.2.0' });
+        return res.status(401).json({ error: 'Invalid API Key, Version 1.2.1' });
     }
     next();
 });
@@ -99,7 +99,7 @@ app.use('/submitShareholdersForm', submitShareholdersFormRoute);
 
 // Default Route
 app.get('/', (req, res) => {
-    res.send('Welcome to the Estock Backend API 1.2.0');
+    res.send('Welcome to the Estock Backend API 1.2.1');
 });
 
 // Error Handling for Undefined Routes
